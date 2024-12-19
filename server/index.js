33940999b132bf5c6c5c2cd90faed8190b9dfc52
Yogesh-A-Gowda <yogeshagowda17575@gmail.com>
2024-12-19 23:41:39 +0530

    Front_end_cors

--- server/index.js
+++ server/index.js
@@ -3,20 +3,21 @@ const app = express();
 const cors =  require('cors');
 const BookModel = require('./models/bookmodel')
 const bookroute = require('./routes/bookRoute')
+const dotenv = require('dotenv')
+dotenv.config()
 
+furl = process.env.FREND;
+console.log(furl)
 app.use(cors(
     {
-        origin : 'http://localhost:5173',
+        origin : furl,
         methods : ['GET','POST','PUT','DELETE'],
-        allowedHeaders : ['Content-Type'],
+        allowedHeaders : ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
     }
 
 ))
-console.log(process.env.FRONT_END);
 app.use(express.json());
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-dotenv.config()
 
 URI = process.env.URI
 PORT = process.env.PORT
