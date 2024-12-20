9ebd78b4c77b42ab6e13e50dce073b83116d9d4c
Yogesh-A-Gowda <yogeshagowda17575@gmail.com>
2024-12-20 11:08:56 +0530

    'code-done'

--- server/index.js
+++ server/index.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const app = express();
-const cors =  require('cors');
-const BookModel = require('./models/bookmodel')
-const bookroute = require('./routes/bookRoute')
-const dotenv = require('dotenv')
-dotenv.config()
+const cors = require('cors');
+const BookModel = require('./models/bookmodel');
+const bookroute = require('./routes/bookRoute');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 furl = process.env.FREND;
-console.log(furl)
+console.log(furl);
+
+// CORS configuration
 app.use(cors(
     {
-        origin : furl,
-        methods : ['GET','POST','PUT','DELETE'],
-        credentials:true
+        origin: furl,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
     }
+));
 
-))
 app.use(express.json());
+
 const mongoose = require('mongoose');
 
-URI = process.env.URI
-PORT = process.env.PORT
+URI = process.env.URI;
+PORT = process.env.PORT;
 
-mongoose.connect(URI).then(console.log('MongoDB Connected')).catch(err => console.log(err));
+mongoose.connect(URI).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
 app.use('/books', bookroute);
 
-
 app.listen(PORT, () => {
-    console.log(`Server running at ${PORT}`)
-})
+    console.log(`Server running at http://localhost:${PORT}`);
+});
