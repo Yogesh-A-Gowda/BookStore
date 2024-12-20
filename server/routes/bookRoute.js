02835cf7f98eb2b5d681ffb541148f9f9d6917fa
Yogesh-A-Gowda <67061755+Yogesh-A-Gowda@users.noreply.github.com>
2024-12-20 12:39:53 +0530

    Update bookRoute.js

--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -4,15 +4,15 @@ const BookModel = require('../models/bookmodel');
 // Setting CORS headers for each route
 const FRONT_END_URL = process.env.FREND;
 
-const setCORSHeaders = (res) => {
+/*const setCORSHeaders = (res) => {
     res.setHeader('Access-Control-Allow-Origin', FRONT_END_URL);  // Frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-};
+};*/
 
 // POST route for creating a book
 router.post('/', async(req, res) => {
-    setCORSHeaders(res); // Set CORS headers before sending the response
+    //setCORSHeaders(res); // Set CORS headers before sending the response
     try {
         if (!req.body.title || !req.body.author || !req.body.publishYear) {
             return res.status(400).send({ message: 'All fields are required' });
@@ -36,7 +36,7 @@ router.post('/', async(req, res) => {
 
 // PUT route for updating a book
 router.put('/:id', async(req, res) => {
-    setCORSHeaders(res); // Set CORS headers before sending the response
+    //setCORSHeaders(res); // Set CORS headers before sending the response
     try {
         if (!req.body.title || !req.body.author || !req.body.publishYear) {
             return res.status(400).send({ message: 'All fields are required' });
@@ -65,7 +65,7 @@ router.put('/:id', async(req, res) => {
 
 // DELETE route for deleting a book
 router.delete('/:id', async(req, res) => {
-    setCORSHeaders(res); // Set CORS headers before sending the response
+    //setCORSHeaders(res); // Set CORS headers before sending the response
     try {
         const { id } = req.params;
         const result = await BookModel.findByIdAndDelete(id);
@@ -80,7 +80,7 @@ router.delete('/:id', async(req, res) => {
 
 // GET route for fetching all books
 router.get('/', async(req, res) => {
-    setCORSHeaders(res); // Set CORS headers before sending the response
+    //setCORSHeaders(res); // Set CORS headers before sending the response
     try {
         const books = await BookModel.find({});
         return res.status(200).json({
@@ -95,7 +95,7 @@ router.get('/', async(req, res) => {
 
 // GET route for fetching a single book by ID
 router.get('/:id', async(req, res) => {
-    setCORSHeaders(res); // Set CORS headers before sending the response
+    //setCORSHeaders(res); // Set CORS headers before sending the response
     try {
         const { id } = req.params;
         const book = await BookModel.findById(id);
