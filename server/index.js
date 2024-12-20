1e3c4d1391c15a6a827ed3ba94d04d863e37ce82
Yogesh-A-Gowda <67061755+Yogesh-A-Gowda@users.noreply.github.com>
2024-12-20 12:48:35 +0530

    Update index.js
    Crude method for deployment, not the proper approach

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ console.log(furl);
 
 // CORS configuration
 const allowedOrigins = [
-  furl, // Local development// Production frontend
+  furl, // Local development
+  'https://book-store-zeta-sable.vercel.app',// Production frontend
 ];
 
 // CORS middleware
